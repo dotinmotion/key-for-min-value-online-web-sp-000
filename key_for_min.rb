3b6d43e88f24670821066350beb7e063fb24305b
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-14 15:26:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest=''
-  sort_array=[]
-  name_hash.each do |key, value|
-    sort_array << value
-  end
-  sort_array.sort do |a,b| {a <=> b}
-  sort_array.first
+  name_hash.sort do |x,y| {x <=> y}
+end.first[0]
   
     
 end
